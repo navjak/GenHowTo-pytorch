ba74dc511b84c846f9bbd019b38af31469df4002
navjak <navdeepjakhar30@gmail.com>
2026-01-29 18:22:26 +0530

    added inference file

---
+++ inference.py
@@ -0,0 +1,95 @@
+import os
+import torch
+import numpy as np
+from PIL import Image
+from diffusers import DDIMScheduler
+import torchvision.transforms as transforms
+
+from model import GenHowToModel
+
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+
+
+def load_model(model_path):
+    model = GenHowToModel()
+    
+    # load weights
+    state_dict = torch.load(model_path, map_location="cpu")
+    model.load_state_dict(state_dict, strict=False)
+    print("Model loaded")
+    
+    model.to(DEVICE)
+    model.eval()
+    return model
+
+
+def generate(model, img_path, prompt, steps=50, skip=2):
+    # Prepare Scheduler (DDIM)
+    scheduler = DDIMScheduler.from_pretrained("Manojb/stable-diffusion-2-1-base", subfolder="scheduler")
+    scheduler.set_timesteps(steps)
+    
+    # preprocessing/transforms
+    img = Image.open(img_path).convert("RGB")
+    transform = transforms.Compose([
+        transforms.Resize(512),
+        transforms.CenterCrop(512),
+        transforms.ToTensor(),
+        transforms.Normalize([0.5], [0.5])
+    ])
+    src_tensor = transform(img).unsqueeze(0).to(DEVICE)
+    
+    # encode src img -> z_src
+    with torch.no_grad():
+        z_src = model.vae.encode(src_tensor).latent_dist.sample() * model.vae.config.scaling_factor
+
+    # start from noisy z_src instead of pure random noise
+
+    timesteps = scheduler.timesteps
+    t_start = timesteps[skip]
+    
+    epsilon = torch.randn_like(z_src)
+    
+    # add noise
+    z_t = scheduler.add_noise(z_src, epsilon, torch.tensor([t_start]))
+    
+    print(f"Starting Inference from step {skip}/{steps} (t={t_start.item()})...")
+    
+    # Sampling loop
+    for i, t in enumerate(timesteps[skip:]):
+        # expand for batch if needed (here batch=1)
+        t_batch = torch.tensor([t], device=DEVICE)
+        
+        with torch.no_grad():
+            pred_noise = model.predict_noise(z_src, z_t, [prompt], t_batch)
+            
+            step_output = scheduler.step(pred_noise, t, z_t) # step
+            z_t = step_output.prev_sample
+
+    # decode image with VAE
+    with torch.no_grad():
+        img_out = model.vae.decode(z_t / model.vae.config.scaling_factor).sample
+        
+    # post processing
+    img_out = (img_out / 2 + 0.5).clamp(0, 1)
+    img_out = img_out.cpu().permute(0, 2, 3, 1).numpy()[0]
+    img_out = (img_out * 255).astype(np.uint8)
+    
+    return Image.fromarray(img_out)
+
+def main():
+    model_path = "/checkpoints/ght_epoch_1.pth"
+    test_img = "/data_dir/i_init/sample1.jpeg"
+    
+    try:
+        model = load_model(model_path)
+    except FileNotFoundError:
+        print(f"ERROR - Model weights file not found.")
+        return
+
+    # prompt for image
+    result = generate(model, test_img, "an apple in two pieces after being cut in half")
+    result.save("gen_img.png")
+    print("Saved generated image")
+
+if __name__ == "__main__":
+    main()
